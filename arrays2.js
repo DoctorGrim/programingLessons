4409eefb3b9bf42bcf1c13116d9a057b7ba2c6ef
DoctorGrim <estlund.marshall@gmail.com>
2020-11-05 17:56:48 -0500

    array prob

---
+++ arrays2.js
@@ -0,0 +1,10 @@
+// Write a function that reverses a string.
+
+const reverseString = (string) => {
+
+  return newString
+};
+
+let newString = reverseString("hello");
+
+console.log(newString);
--- examples.js
+++ examples.js
@@ -16,3 +16,35 @@ let outOf100 = function (val) {
     }
     
 }
+
+
+
+
+const reverseString = (string) => {
+    let array = string.split("");
+    let left = 0;
+    let right = array.length;
+  
+    while (left < right) {
+      let temp = array[left];
+      array[left] = array[right];
+      array[right] = temp;
+      left++;
+      right--;
+    }
+    let newString = array.join("")
+    return newString
+  };
+  
+
+  const reverseString = (string) => {
+    let array = string.split("");
+    let newArray=[];
+    let loopTime= array.length
+    for (let index = 0; index < loopTime; index++) {
+      temp = array.pop();
+      newArray.push(temp)
+      }
+    let newString = newArray.join("")
+    return newString
+  };
