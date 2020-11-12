f1e2663ef79b39fd1905e04560d60bff4dbf9a1f
DoctorGrim <estlund.marshall@gmail.com>
2020-11-12 16:31:02 -0500

    added leetcode

--- examples.js
+++ examples.js
@@ -48,3 +48,23 @@ const reverseString = (string) => {
     let newString = newArray.join("")
     return newString
   };
+
+
+  const kidsWithCandies = (candies, extraCandies) => {
+    const ret = [];
+    let max = 0;
+    for (const val of candies) {
+      val > max && (max = val);
+    }
+    for (let i = 0; i < candies.length; ++i) {
+      ret.push(candies[i] + extraCandies >= max);
+    }
+    return ret;
+  };
+
+  const kidsWithCandies = (candies, extraCandies) => {
+    const max = Math.max(...candies);
+    return candies.map(candy => candy + extraCandies >= max);
+}
+
+const kidsWithCandies = (candies, extraCandies, max = Math.max(...candies)) => candies.map(candy => candy + extraCandies >= max);
---
+++ leet.js
@@ -0,0 +1,12 @@
+// Given array candies and the integer extraCandies where each index of the array repersents a kid with its value being how much candy they have,
+// find out if each kid could potentially have the most candy if given all the extras.
+
+const kidsWithCandies = (candies, extraCandies) => {
+
+    return "somthing";
+  };
+
+
+let kids = kidsWithCandies([2,3,5,1,3], 3)
+
+console.log(kids)
